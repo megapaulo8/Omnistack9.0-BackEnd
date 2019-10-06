cf2647029ede08539d85a78ef5d1b71297b38969
megapaulo8 <paulosantos194@hotmail.com>
2019-10-06 16:31:59 -0300

    feat : socket.io client

--- src/server.js
+++ src/server.js
@@ -2,14 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-s6vtb.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
 
 //req.query = Acessar query params (para filtros)
 //req.params = Acessar route params (para edição e delete)
@@ -20,4 +39,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
